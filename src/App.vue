73d74f3975f03af902c9a1c105640c5cfa69e913
Chezzby <itzchezvil17@gmail.com>
2024-07-20 22:18:24 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <learning-rosource
+      v-for="res in storedResource"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-rosource>
+  </ul>
+</template>
+
+<script>
+import LearningRosource from './components/learning-resources/LearningResource.vue';
+
+export default {
+  components: {
+    LearningRosource,
+  },
+  data() {
+    return {
+      storedResource: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+      ],
+    };
+  },
+};
+</script>
