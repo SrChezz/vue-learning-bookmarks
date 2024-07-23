254096de36bd0143ca3c3b910548b0d004e32812
Chezzby <itzchezvil17@gmail.com>
2024-07-23 10:20:02 -0500

    finished basic project

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,35 @@
 <template>
-  <ul>
-    <learning-rosource
-      v-for="res in storedResource"
-      :key="res.id"
-      :title="res.title"
-      :description="res.description"
-      :link="res.link"
-    ></learning-rosource>
-  </ul>
+  <the-header title="Remember Me"></the-header>
+  <the-resources></the-resources>
 </template>
 
 <script>
-import LearningRosource from './components/learning-resources/LearningResource.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
 
 export default {
   components: {
-    LearningRosource,
+    TheHeader,
+    TheResources,
   },
   data() {
-    return {
-      storedResource: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation',
-          link: 'https://vuejs.org',
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'The official Vue.js documentation',
-          link: 'https://vuejs.org',
-        },
-      ],
-    };
+    return {};
   },
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
